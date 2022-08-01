e20b83d907735ad85cd305671b94f421879d30aa
rizk <arezk95@gmail.com>
2022-08-01 13:18:39 +0200

    network modifications + k8 file update

--- network/router.tf
+++ network/router.tf
@@ -6,15 +6,3 @@ resource "google_compute_router" "router" {
     asn = 64514
   }
 }
-
-
-# additional to make jenkins go only WILL DELETE LATER
-resource "google_compute_router" "router_gke" {
-  name    = "my-router-gke"
-  region  = google_compute_subnetwork.restricted-subnet.region
-  network = google_compute_network.vpc_network.id
-  bgp {
-    asn = 64514
-  }
-}
-
